124216b1f6f722ced70821306bcb78421bea229c
biral-pradhan <biralkumarpradhan@gmail.com>
2020-12-30 12:43:04 +0530

    Adding Gate Level Modelling

--- src/verilog_master_app/urls.py
+++ src/verilog_master_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('verilog-basics-1/',views.verilogbasics1, name="verilogbasics1"),
     path('digital-basics/',views.digital_basics,name="digital_basics"),
     path('modules-and-port-mappings/',views.modules_and_port_mappings, name="modules_and_port_mappings"),
+    path('gate-level-modelling/',views.gate_level_modelling, name="gate_level_modelling"),
     path('behavioral-2/',views.behavioral_2,name="behavioral2"),
 
 ]
