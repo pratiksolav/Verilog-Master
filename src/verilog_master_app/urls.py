91caa7a22f901889313eda4ed73af7c00d0ff2c0
Pratik Solav <pratiksolav29@gmail.com>
2021-02-27 11:55:22 +0530

    Added upto Parity Generation

--- src/verilog_master_app/urls.py
+++ src/verilog_master_app/urls.py
@@ -7,15 +7,21 @@ urlpatterns = [
     path('intro1/',views.intro1,name="intro1"),
     path('intro2/',views.intro2,name="intro2"),
     path('verilog-basics-1/',views.verilogbasics1, name="verilogbasics1"),
+    path('verilog-basics-2/',views.verilog_basics_2, name="verilog_basics_2"),
     path('digital-basics/',views.digital_basics,name="digital_basics"),
     path('modules-and-port-mappings/',views.modules_and_port_mappings, name="modules_and_port_mappings"),
+    path('types_of_modelling/',views.types_of_modelling,name="types_of_modelling"),
     path('gate-level-modelling/',views.gate_level_modelling, name="gate_level_modelling"),
+    path('dataflow_level_modelling/',views.dataflow_level_modelling, name="dataflow_level_modelling"),
     path('behavioral-2/',views.behavioral_2,name="behavioral2"),
     path('half-adder/',views.half_adder,name="half_adder"),
     path('full-adder/',views.full_adder,name="full_adder"),
     path('full-half-adder/',views.full_half_adder,name="full_half_adder"),
     path('half-subtractor/',views.half_subtractor,name="half_subtractor"),
     path('mux/',views.mux,name="mux"),
-
+    path('bin_to_bcd/',views.bin_to_bcd,name="bin_to_bcd"),
+    path('bin_to_gray/',views.bin_to_gray,name="bin_to_gray"),
+    path('bin_to_xs3/',views.bin_to_xs3,name="bin_to_xs3"),
+    path('parity_generator/',views.parity_generator,name="parity_generator"),
 
 ]
--- src/verilog_master_app/views.py
+++ src/verilog_master_app/views.py
@@ -36,6 +36,12 @@ def digital_basics(request):
 def modules_and_port_mappings(request):
 	return render(request, 'articles/modules-and-port-mappings.html')
 
+def types_of_modelling(request):
+	return render(request, 'articles/types_of_modelling.html')
+
+def dataflow_level_modelling(request):
+	return render(request, 'articles/dataflow-level-modelling.html')
+
 def gate_level_modelling(request):
 	return render(request, 'articles/gate-level-modelling.html')
 
@@ -53,6 +59,14 @@ def full_half_adder(request):
 
 def half_subtractor(request):
 	return render(request, 'articles/half-subtractor.html')
-
+	
 def mux(request):
 	return render(request,'articles/mux.html')
+def bin_to_bcd(request):
+	return render(request,'articles/bin_to_bcd.html')
+def bin_to_gray(request):
+	return render(request,'articles/bin_to_gray.html')
+def bin_to_xs3(request):
+	return render(request,'articles/bin_to_xs3.html')
+def parity_generator(request):
+	return render(request,'articles/parity_generator.html')
